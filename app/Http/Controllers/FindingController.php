97c02aef64ea507cfdbd370336bb7ecd6783f493
minttu <minttu.koponen@gmail.com>
2017-09-27 14:41:35 +0300

    FindingController and api.php

---
+++ app/Http/Controllers/FindingController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Finding;
+
+
+class FindingController extends Controller
+{
+    public function getAll(){
+        return Finding::all();
+    }
+
+    public function getId($id){
+        return Finding::find($id);
+    }
+
+    public function getBerry($berry){
+        return Finding::find($berry);
+    }
+
+    public function add(Request $request){
+        return Article::create($request->all());
+    }
+
+    public function delete(Request $request, $id){
+        $finding = Finding::findOrFail($id);
+        $finding->delete();
+        return 204;
+    }
+
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use App\Finding;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +17,12 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 
-    //Route::get('\events', 'MyApi@ebentList')
+    Route::get('\findings', 'FindingController@getAll');
+    Route::get('\findings/{id}', 'FindingController@getId');
+    Route::get('\findings/{berry}', 'FindingController@getBerry');
+    Route::post('\findings', 'FindingController@add');
+    Route::delete('\findings/{id}', 'FindingController@delete');
+
+
+
 });
