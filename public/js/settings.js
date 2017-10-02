ddda9bddbfb0ce09e09e7372161e147c7e493681
Saini Patala <saini.patala@gmail.com>
2017-10-02 11:17:45 +0300

    settings to gitignore

--- public/js/settings.js
+++ public/js/settings.js
@@ -3,5 +3,5 @@
  */
 
 
-//var address = "http://localhost:8888/rest/public/api"; //Sainin localhost
-var address = "http://localhost/rest/public/api"; //Mintun localhost
+var address = "http://localhost:8888/rest/public/api"; //Sainin localhost
+//var address = "http://localhost/rest/public/api"; //Mintun localhost
