070d7a4477e64fb14ceca2ae14bdcda0bf127754
minttu <minttu.koponen@gmail.com>
2017-10-02 20:10:26 +0300

    tried to add a unit test but require path failing

--- tests/Unit/BerryTest.php
+++ tests/Unit/BerryTest.php
@@ -4,9 +4,11 @@ namespace Tests\Unit;
 
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+require_once '../../app/Http/Controllers/BerryController.php'; //tätä ei löydä???
 
 class BerryTest extends TestCase
 {
+    // to run: vendor/bin/phpunit.bat
     /**
      * A basic test example.
      *
@@ -17,4 +19,7 @@ class BerryTest extends TestCase
         $this->assertTrue(true);
     }
 
+    public function testGetId(){
+        $this->assertEquals(1, getId("Puolukka"));
+    }
 }
